173c2727856899664e786a309bf26269d910c5a9
pepinovimr <pepino.vimr@seznam.cz>
2024-01-22 15:15:32 +0100

    Updated DataValidationBuilderFactory to work with DataValidationBuilder abstract class

--- DomainLayer/Validations/DataValidations/DataValidationBuilderFactory.cs
+++ DomainLayer/Validations/DataValidations/DataValidationBuilderFactory.cs
@@ -9,9 +9,9 @@ namespace DomainLayer.Validations.DataValidations
             validation.GetType() switch
             {
                 Type t when t == typeof(LeaderValidation) => new LeaderValidationBuilder(Record, validation),
-                Type t when t == typeof(ControlFieldValidation) => new ControlFieldValidationBuilder(Record),
-                Type t when t == typeof(DataFieldValidation) => new DataFieldValidationBuilder(Record),
-                Type t when t == typeof(SubFieldValidation) => new SubFieldValidationBuilder(Record),
+                Type t when t == typeof(ControlFieldValidation) => new ControlFieldValidationBuilder(Record, validation),
+                Type t when t == typeof(DataFieldValidation) => new DataFieldValidationBuilder(Record, validation),
+                Type t when t == typeof(SubFieldValidation) => new SubFieldValidationBuilder(Record, validation),
                 _ => throw new ArgumentException("Unexpected type of Validation")
             };
     }
